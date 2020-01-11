00b0f09372eca46ce1170b55ded7de0f4ea24dec
Sohel Amin <sohelamincse@gmail.com>
2020-01-11 13:17:37 +0600

    Update docker visualizer

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const os = require('os');
 
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world from Docker Swarm\n');
+  res.send(`Hello world from Docker Swarm [${os.hostname}]`);
 });
 
 app.listen(PORT, HOST);
